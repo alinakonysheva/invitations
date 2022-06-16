b8eb39b48802f70348b0b324f4b2fb7ac34978c1
Alina Konysheva <konysheva.alina@gmail.com>
2022-06-16 23:10:55 +0200

    user

--- register/views.py
+++ register/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
-from django.contrib.auth.forms import UserCreationForm
 from .forms import RegisterForm
 
 
@@ -9,10 +8,8 @@ def register(response):
         form = RegisterForm(response.POST)
         if form.is_valid():
             form.save()
-            return redirect("/home")
-        else:
-            form = RegisterForm(response.POST)
-            return render(response, "register/../templates/registration/register.html", {"form": form})
+        return redirect("/home")
+
     else:
         form = RegisterForm(response.POST)
-        return render(response, "register/../templates/registration/register.html", {"form": form})
+        return render(response, "register/register.html", {"form": form})
