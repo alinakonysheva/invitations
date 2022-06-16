78ac581a16a413e5d8ab9bf47c69f7b98474c179
Alina Konysheva <konysheva.alina@gmail.com>
2022-06-16 21:53:04 +0200

    user

--- invitations/urls.py
+++ invitations/urls.py
@@ -3,8 +3,9 @@ from . import views
 
 urlpatterns = [path(f"<int:id>", views.index, name="index"),
                path(f"home/", views.home, name="home"),
+               path(f"", views.home, name="home"),
                path(f"groups/", views.groups, name="groups"),
-               path(f"groups/create_groups/", views.create_groups, name="create_groups"),
+               path(f"groups/create_groups", views.create_groups, name="create_groups"),
                path(f"events/", views.events, name="events"),
                path(f"templates/", views.templates, name="templates"),
                path(f"guests/", views.guests, name="guests"),
