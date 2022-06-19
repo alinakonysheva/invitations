b6b9482ae83d6b489e33ebbc8ec97de4f9b059f0
Alina Konysheva <konysheva.alina@gmail.com>
2022-06-19 11:05:29 +0200

    started templates

--- invitations/models.py
+++ invitations/models.py
@@ -1,12 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Group(models.Model):
     name = models.CharField(max_length=200)
+    #user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="group", null=True)
 
     def __str__(self):
         return self.name
-
+# итерироваться по группам надо будет так for group in user.group.all
 
 class Guest(models.Model):
     group = models.ForeignKey(Group, on_delete=models.CASCADE)
@@ -28,6 +30,7 @@ class Event(models.Model):
     group = models.ForeignKey(Group, on_delete=models.CASCADE)
     template = models.ForeignKey(Template, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
+    #user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="event", null=True)
 
     def __str__(self):
         return self.name
